8dbb2fbc353d304c045f37b55c829b80d0284818
Job Karani <jobkarani084j@gmail.com>
2023-01-10 22:30:15 +0300

    creates navbar and footer components

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import  "./footer.css"
+
+export default function Footer(){
+    return(
+        <div>
+            <a href="mailto:" className="email">jobkarani084j@gmail.com</a>
+        </div>
+    )
+}
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <>
+        <h1>Home</h1>
+        <h1>About</h1>
+        </>
+    )
+}
